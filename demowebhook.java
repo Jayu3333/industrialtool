9d95cf3496f5a9f12d06e252995739a6a2170aa8
jayu3333 <jayantpatle4@gmail.com>
2024-06-14 12:10:28 +0530

    demowebhook file committed

--- demowebhook.java
+++ demowebhook.java
@@ -3,7 +3,7 @@ public class demowebhook{
 public static void main(String args[])
 
 {
-System.out.printIn("This file is trigger to Jenkins");
+System.out.println("This file is trigger to Jenkins");
 }
 
 }
