6c8ec5cf07323a10f8c615aa0bb9d3c38feedcf0
jayu3333 <jayantpatle4@gmail.com>
2024-06-15 20:25:34 +0530

    file committed by jayu

--- webhook3.java
+++ webhook3.java
@@ -3,7 +3,7 @@ public class webhook3{
 public static void main(String args[])
 
 {
-System.out.printIn("This file is trigger to Jenkins");
+System.out.println("This file is trigger to Jenkins");
 }
 
 }
